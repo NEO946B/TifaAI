2c1677c24301bea0d6e79fabbb7475c98ae07a7e
Yellow <neo.huang3@gmail.com>
2020-08-21 22:02:34 +0800

    ad some folder

---
+++ core/entities/smart_area.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+class SmartArea(object):
+    """
+    The location in the space where NPCs can make some intellient bahaviors.
+    Ref: 《Smart Areas: A modular approach to simulation of daily life in an open world video game》
+    """
+    def __init__(self):
+        pass
+
